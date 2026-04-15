ace9bb29657184e32eded205f1b3dd9140359a05
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 15:55:43 +0400

    Solving Task15 - Reverse and Count Text Using Loops in Java Strings

--- src/StringReverseDemo.java
+++ src/StringReverseDemo.java
@@ -6,6 +6,27 @@ public class StringReverseDemo {
         System.out.println(name.length());
 
 
+        for(int i = 0; i < name.length(); i++){
+            System.out.println(name.charAt(i));
+
+        }
+
+        String reversed = "";
+        for(int i = name.length() - 1; i >= 0; i--){
+            reversed = reversed + name.charAt(i);
+        }
+
+        System.out.println(reversed);
+
+        char cr = 'a';
+        int count = 0;
+
+        for(int i = 0; i < name.length(); i++){
+            if(name.charAt(i) == cr){
+                count++;
+            }
+        }
+        System.out.println("Character" + cr + "appears: " + count + "times");
 
     }
 }
