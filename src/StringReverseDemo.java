9bfeb852dec3be552664999612bcbed6865e5583
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 15:45:09 +0400

    Solving Task15 Part1

---
+++ src/StringReverseDemo.java
@@ -0,0 +1,11 @@
+public class StringReverseDemo {
+    public static void main(String[] args){
+
+        String name = "Mawadda";
+        System.out.println(name);
+        System.out.println(name.length());
+
+
+
+    }
+}
