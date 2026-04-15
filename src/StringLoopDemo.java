452963920bb565eddbde5d4a34e9bed128a175ca
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 15:01:04 +0400

    Solving Task11 Part2

--- src/StringLoopDemo.java
+++ src/StringLoopDemo.java
@@ -1,9 +1,31 @@
 public class StringLoopDemo {
     public static void main(String[] args){
-        String title = "The Dark Room";
+        String title = "The Dark Room 123";
         System.out.println(title);
         System.out.println(title.length());
 
+        int vowels = 0;
+        int consonants = 0;
+        int digits = 0;
+
+        System.out.println("\nCharacters in the string:");
+        for(int i = 0; i < title.length(); i++){
+
+            char c = title.charAt(i);
+            System.out.println(c);
+
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+                vowels++;
+            }
+            else if(Character.isLetter(c)){
+                consonants++;
+            }
+            else if(Character.isDigit(c)){
+                digits++;
+            }
+
+        }
 
 
 
