05c2ad99a2fc350b27ea4c3cbb83e613e0619b76
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 12:08:53 +0400

    Solving Task1 part1

---
+++ src/StringOperationsDemo.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+
+public class StringOperationsDemo {
+    public static void main(String[] args){
+
+        String name = "Mawadda";
+
+        System.out.println("Name: " + name);
+
+        System.out.println("\n--- Length --- "+ name.length());
+
+        System.out.println("\n---Uppercase--- " + name.toUpperCase());
+        System.out.println("\n---Lowercase--- " + name.toLowerCase());
+
+
+    }
+
+}
