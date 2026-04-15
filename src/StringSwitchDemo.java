b615c75d8e44e4ca9b21cb11501bc6a33a97ff2d
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 16:26:56 +0400

    Solving Task18 -: Use Switch Statement with Java Strings

---
+++ src/StringSwitchDemo.java
@@ -0,0 +1,26 @@
+public class StringSwitchDemo {
+    public static void main(String[] args){
+
+        String Department = "Electronics";
+        System.out.println(Department);
+
+        switch(Department){
+
+            case "Electronics":
+                System.out.println("Electronics Department selected");
+                break;
+
+            case "HR":
+                System.out.println("HR Department selected");
+                break;
+
+            case "IT":
+                System.out.println("IT Department selected");
+                break;
+
+            default:
+
+
+        }
+    }
+}
