8ff6c40f5f288e39d684a3b71e456929d6ff8cda
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 12:22:32 +0400

    Splving Task2 -Extract and Compare Text Using Java String Methods

---
+++ src/StringCompareDemo.java
@@ -0,0 +1,17 @@
+public class StringCompareDemo {
+    public static void main(String[] args){
+
+        String name = "Mawadda";
+
+        System.out.println(name);
+
+        System.out.println(name.charAt(2));
+
+        System.out.println(name.substring(4));
+
+        String name2 = "Jenan";
+
+        System.out.println(name.equals(name2));
+
+    }
+}
