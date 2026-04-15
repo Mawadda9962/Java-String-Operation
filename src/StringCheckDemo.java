c04b0cffff27d3ad38a78a076ec427a7197588ea
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 14:08:21 +0400

    Solving Task6 - Extract, Replace, and Check Text in Java Strings

---
+++ src/StringCheckDemo.java
@@ -0,0 +1,28 @@
+public class StringCheckDemo {
+    public static void main(String[] args){
+
+        String title = "Java Programming Language";
+
+        System.out.println(title);
+
+
+        System.out.println(title.substring(4));
+
+        System.out.println(title.replace("Java", "C++"));
+
+        System.out.println(title.contains("Java"));
+
+        System.out.println(title.charAt(1));
+
+        String title2 = "C++ Programming Language";
+
+        System.out.println(title2);
+
+        System.out.println(title.concat(title2));
+
+
+
+
+
+    }
+}
