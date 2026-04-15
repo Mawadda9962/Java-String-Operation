609c889a389c51d0ae873d886fe23b386e141d78
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 16:56:56 +0400

    Solving Task19 -Create a Menu-Driven Java Program for String Operations

--- src/StringMenuDemo.java
+++ src/StringMenuDemo.java
@@ -16,6 +16,50 @@ public class StringMenuDemo {
         System.out.println("5.Compare with another string");
         System.out.print("Enter your choice (1-5): ");
 
+        int userChoice = input.nextInt();
+        input.nextLine();
+
+        switch (userChoice){
+            case 1:
+                System.out.println("Length: " + text.length());
+                break;
+
+            case 2:
+                System.out.println("UPPERCASE: " + text.toUpperCase());
+                break;
+
+            case 3:
+                System.out.println("lowercase: " + text.toLowerCase() );
+                break;
+
+            case 4:
+                System.out.println("Enter the word: ");
+                String word = input.nextLine();
+
+                if (text.contains(word)){
+                    System.out.println("The String contain: " + word);
+                }else{
+                    System.out.println("The String does not contain: " + word);
+                }
+                break;
+
+            case 5:
+                System.out.print("Enter another string: ");
+                String other = input.nextLine();
+
+                if(text.equalsIgnoreCase(other)){
+                    System.out.println("Both strings are equal (ignore case).");
+                } else {
+                    System.out.println("Strings are NOT equal.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid menu choice. Please select 1-5.");
+        }
+
+        }
+
 
     }
-}
+
