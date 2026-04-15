543f438cb6d9ed6f4ba064d8fa09a9da5db633f7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 12:42:07 +0400

    Solving Task4 -Find Positions and Check String Start or End in Java

---
+++ src/StringPositionDemo.java
@@ -0,0 +1,14 @@
+public class StringPositionDemo {
+    public static void main(String[] args){
+
+        String text = "Java String Operation";
+
+        System.out.println(text);
+
+        System.out.println(text.indexOf("String"));
+
+        System.out.println(text.startsWith("Java"));
+
+        System.out.println(text.endsWith("Operation"));
+    }
+}
