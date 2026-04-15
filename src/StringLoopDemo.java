b06422bb06d59a9394bd909d1b4bf4301a71270a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 14:50:50 +0400

    Solving Task11 part1

---
+++ src/StringLoopDemo.java
@@ -0,0 +1,11 @@
+public class StringLoopDemo {
+    public static void main(String[] args){
+        String title = "The Dark Room";
+        System.out.println(title);
+        System.out.println(title.length());
+
+
+
+
+    }
+}
