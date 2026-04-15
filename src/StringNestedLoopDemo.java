a667ca7cee36bf3bf573958afb5b8616a29524d0
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 15:32:06 +0400

    Solving Task14 part1

---
+++ src/StringNestedLoopDemo.java
@@ -0,0 +1,13 @@
+public class StringNestedLoopDemo {
+    public static void main(String[] args){
+
+        String text1 = "Java Programming Language";
+        String text2 = "C++ Programming Language";
+
+        System.out.println(text1);
+        System.out.println(text2);
+
+        System.out.println(text1.length());
+        System.out.println(text2.length());
+    }
+}
