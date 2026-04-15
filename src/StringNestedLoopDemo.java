8a44ee673f806a1e59f0c4f9cff50eb0919e0c67
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 15:43:01 +0400

    Solving Task14 -Use Nested Loops with Java Strings

--- src/StringNestedLoopDemo.java
+++ src/StringNestedLoopDemo.java
@@ -9,5 +9,26 @@ public class StringNestedLoopDemo {
 
         System.out.println(text1.length());
         System.out.println(text2.length());
+
+        int count = 0;
+
+        for(int i = 0; i < text1.length(); i++){
+            for (int j = 0; j < text2.length(); j++){
+
+                if(text1.charAt(i) == text2.charAt(j)){
+                    System.out.println(text1.charAt(i));
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+
+        for(int i = 0; i < text1.length(); i++){
+            for(int j = 0; j < text2.length(); j++){
+
+                System.out.println(text1.charAt(i) + " vs " + text2.charAt(j));
+            }
+        }
+
     }
 }
