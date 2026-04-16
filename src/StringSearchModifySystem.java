2fcfe82a280589fa721dd373e029e86c039526f9
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-16 09:12:49 +0400

    Solving Task22 -Build a Java String Search and Modification Program

--- src/StringSearchModifySystem.java
+++ src/StringSearchModifySystem.java
@@ -8,16 +8,44 @@ public class StringSearchModifySystem {
         System.out.println("Enter your name: ");
         String text = input.nextLine();
 
-        System.out.println(text);
+        System.out.println("Original String: " + text);
 
-        Scanner word = new Scanner(System.in);
 
         System.out.println("Enter the word for searching: ");
-        String Word = input.nextLine();
-
-        System.out.println(Word);
+        String word = input.nextLine();
 
 
+        System.out.println("Word found: " + text.contains(word));
 
+
+        System.out.println("Enter the word to replace: ");
+        String oldWord = input.nextLine();
+
+        System.out.println("Enter the new word: ");
+        String newWord = input.nextLine();
+
+        String modifiedText = text.replace(oldWord, newWord);
+        System.out.println("Modified String: " + modifiedText);
+
+
+        if (modifiedText.length() >= 5) {
+            System.out.println("Substring: " + modifiedText.substring(0, 5));
+        } else {
+            System.out.println("String too short for substring.");
+        }
+
+        if (modifiedText.length() > 0) {
+            System.out.println("First character: " + modifiedText.charAt(0));
+        } else {
+            System.out.println("String is empty.");
+        }
+
+        if (modifiedText.length() > 8) {
+            System.out.println("The modified string is long.");
+        } else {
+            System.out.println("The modified string is short.");
+        }
+
+        input.close();
     }
 }
