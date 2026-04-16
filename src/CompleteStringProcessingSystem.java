eab2680b1fbb2e8cbc3f001b8416d7c2dc5607af
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-16 09:36:35 +0400

    Solving task25 part1

---
+++ src/CompleteStringProcessingSystem.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CompleteStringProcessingSystem {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your full name: ");
+        String text = input.nextLine();
+
+        System.out.println(text);
+
+
+
+    }
+}
