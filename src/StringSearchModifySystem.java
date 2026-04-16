484e4d0c6ad2e952f1264950d78ef2a0b421ee51
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-16 09:06:03 +0400

    Solving Tas23 Part1

---
+++ src/StringSearchModifySystem.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StringSearchModifySystem {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String text = input.nextLine();
+
+        System.out.println(text);
+
+        Scanner word = new Scanner(System.in);
+
+        System.out.println("Enter the word for searching: ");
+        String Word = input.nextLine();
+
+        System.out.println(Word);
+
+
+
+    }
+}
