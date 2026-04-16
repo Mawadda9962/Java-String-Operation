176a0257811dc489423228275fb9699ddca17b38
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-16 09:31:56 +0400

    Solving Part3

--- src/StringLoopValidationSystem.java
+++ src/StringLoopValidationSystem.java
@@ -10,10 +10,22 @@ public class StringLoopValidationSystem {
         System.out.println(text);
         System.out.println(text.length());
 
+
+        int vowCount = 0;
+
         for(int i = 0; i <text.length(); i++){
             char a = text.charAt(i);
             System.out.println(a);
+
+
+            if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' ||
+                    a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U') {
+                vowCount++;
+            }
+        }
+
+        System.out.println(vowCount);
         }
     }
 
-}
+
